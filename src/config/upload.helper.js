c90001962ba6aa19a1664d8af9d43ffbe92512f2
Pradum <satendarpradum@gmail.com>
2024-06-04 09:59:12 +0530

    Created S3 and Upload Object

---
+++ src/config/upload.helper.js
@@ -0,0 +1,22 @@
+import multerS3 from 'multer-s3';
+import multer from 'multer';
+import s3 from './s3.js';
+
+const upload=multer({
+    storage:multerS3({
+        s3:s3,
+        acl:'public-read',
+        bucket:process.env.AWS_BUCKET_NAME,
+        metadata:(req,file,cb)=>{
+            cb(null,{fieldName:file.fieldname});
+        },
+        key:(req,file,cb)=>{
+            const fileName=`${Date.now()}_${Math.round(Math.random()*1E9)}`
+  
+            cb(null,`${fileName}${path.extname(file.originalname)}`);
+
+        }
+    })
+})
+
+export default upload;
